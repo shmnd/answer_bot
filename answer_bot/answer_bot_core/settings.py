259e6bd05c3469f4cb21d4c8583a7dd10d9b526a
Shamnad <hamzashamnad123@gmail.com>
2025-05-05 17:05:06 +0530

    core module settup

--- answer_bot/answer_bot_core/settings.py
+++ answer_bot/answer_bot_core/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 LOCAL_APPS = [
+    'apps.core'
 
 ]
 
--- answer_bot/answer_bot_core/urls.py
+++ answer_bot/answer_bot_core/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, re_path, include
+from django.conf import settings
+from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('apps.core.urls')),
+
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]

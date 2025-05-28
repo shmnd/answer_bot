1b7a0bb44e17255b9f6d20a2753822d23c0f1441
Shamnad <hamzashamnad123@gmail.com>
2025-05-28 15:32:04 +0530

    elastic client

--- answer_bot/answer_bot_core/helpers/elastic_client.py
+++ answer_bot/answer_bot_core/helpers/elastic_client.py
@@ -1,12 +1,9 @@
 from elasticsearch import Elasticsearch
 from django.conf import settings
 
-PASSWORD = settings.ELASTIC_USER_PASS
-ES_CA_CERT_PATH = settings.CA_CERTS_PATH
-
 es = Elasticsearch(
     "https://localhost:9200",
-    basic_auth=("elastic", PASSWORD),
-    verify_certs=True, # "False" Use only for local development; for prod, set up certs properly
-    ca_certs=ES_CA_CERT_PATH,
+    basic_auth=("elastic", settings.ELASTIC_USER_PASS),
+    verify_certs=True,
+    ca_certs=settings.CA_CERTS_PATH,
 )

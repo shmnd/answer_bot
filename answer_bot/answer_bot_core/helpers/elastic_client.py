df5c1c8af7b2603ff47863ca3539625bd43b9ab6
Shamnad <hamzashamnad123@gmail.com>
2025-05-28 13:34:39 +0530

    ADDED ELASTIC SEARCH PATH

--- answer_bot/answer_bot_core/helpers/elastic_client.py
+++ answer_bot/answer_bot_core/helpers/elastic_client.py
@@ -1,10 +1,12 @@
 from elasticsearch import Elasticsearch
 from django.conf import settings
 
-password = settings.ELASTIC_USER_PASS
+PASSWORD = settings.ELASTIC_USER_PASS
+ES_CA_CERT_PATH = settings.CA_CERTS_PATH
 
 es = Elasticsearch(
     "https://localhost:9200",
-    basic_auth=("elastic", password),
+    basic_auth=("elastic", PASSWORD),
     verify_certs=True, # "False" Use only for local development; for prod, set up certs properly
+    ca_certs=ES_CA_CERT_PATH,
 )

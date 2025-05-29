9021659b500fef4a238f4e7a896564ea0b16b946
Shamnad <hamzashamnad123@gmail.com>
2025-05-29 15:39:43 +0530

    cert false

--- answer_bot/answer_bot_core/helpers/elastic_client.py
+++ answer_bot/answer_bot_core/helpers/elastic_client.py
@@ -4,6 +4,6 @@ from django.conf import settings
 es = Elasticsearch(
     "http://localhost:9200",
     basic_auth=(settings.ELASTIC_USER, settings.ELASTIC_USER_PASS),
-    verify_certs=True,
-    ca_certs=settings.CA_CERTS_PATH,
+    verify_certs=False,
+    # ca_certs=settings.CA_CERTS_PATH,
 )

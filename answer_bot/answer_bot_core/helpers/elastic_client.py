b0264564aa1ae6e1e45c3c4e1a1c706e594e6883
Shamnad <hamzashamnad123@gmail.com>
2025-05-29 14:57:27 +0530

    elactic client changes

--- answer_bot/answer_bot_core/helpers/elastic_client.py
+++ answer_bot/answer_bot_core/helpers/elastic_client.py
@@ -1,9 +1,10 @@
 from elasticsearch import Elasticsearch
 from django.conf import settings
+import requests
 
 es = Elasticsearch(
     "http://localhost:9200",
     basic_auth=(settings.ELASTIC_USER, settings.ELASTIC_USER_PASS),
-    verify_certs=True,
+    verify_certs=False,
     ca_certs=settings.CA_CERTS_PATH,
 )

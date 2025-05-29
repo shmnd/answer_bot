b1cb1c1f01dbdc93a4576cec86c504d7ed05b6da
Shamnad <hamzashamnad123@gmail.com>
2025-05-29 13:10:18 +0530

    https

--- answer_bot/answer_bot_core/helpers/elastic_client.py
+++ answer_bot/answer_bot_core/helpers/elastic_client.py
@@ -2,7 +2,7 @@ from elasticsearch import Elasticsearch
 from django.conf import settings
 
 es = Elasticsearch(
-    "http://localhost:9200",
+    "https://localhost:9200",
     basic_auth=(settings.ELASTIC_USER, settings.ELASTIC_USER_PASS),
     verify_certs=True,
     ca_certs=settings.CA_CERTS_PATH,
